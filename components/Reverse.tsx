8dd1f24184da99e3b7d414f82395f854709948b4
Hoyasumii <alanreisanjo@gmail.com>
2024-09-11 15:55:24 -0300

    Ajustes

--- components/Reverse.tsx
+++ components/Reverse.tsx
@@ -2,8 +2,17 @@ import { Children } from "react";
 
 type Props = {
   children: React.ReactNode;
+  defaultMode?: boolean;
 };
 
-export function Reverse({ children }: Props) {
-  return Children.toArray(children).reverse().map((item) => item);
+export function Reverse({ children, defaultMode = true }: Props) {
+  if (defaultMode) {
+    return Array.from(children as React.ReactElement[])
+      .reverse()
+      .map((item) => item);
+  }
+
+  return Children.toArray(children)
+    .reverse()
+    .map((item) => item);
 }
